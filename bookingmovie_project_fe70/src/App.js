10fbf22c23c22d4853bfcddf96da6112e1bf1f1a
Silver Wolf <84911530+Khoa-sudo@users.noreply.github.com>
2022-06-15 15:09:47 +0700

    update_branch

--- bookingmovie_project_fe70/src/App.js
+++ bookingmovie_project_fe70/src/App.js
@@ -1,17 +1,10 @@
 import "./App.css";
-import BaiTapTongHop from "./BaiTapTongHop/BaiTapTongHop";
-import Demo from "./Lodash/Demo";
-import BaiTapTongHopTailWind from "./TailWindComponent/BaiTapTongHopTailWind";
-import DemoGrid from "./TailWindComponent/DemoGrid";
-import FlexDemo from "./TailWindComponent/FlexDemo";
-import PaddingMarginDemo from "./TailWindComponent/PaddingMarginDemo";
+
 
 function App() {
-  return (
-    <div className="App">
-     
-    </div>
-  );
+  return <div className="App">
+    
+  </div>;
 }
 
 export default App;
